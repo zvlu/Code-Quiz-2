e1310fc6f8db424482b6128b4e2f20551fa3553c
ZVLU <zvluwork@gmail.com>
2019-12-31 09:18:44 -0500

    Finished the quiz working on highscore and timer

--- Assets/script.js
+++ Assets/script.js
@@ -1,55 +1,93 @@
 
+// GLOBAL VARIABLES
 
-
-let score =0;
+let score = 0;
 let currentQuestion = 0;
 
 // Object of Questions
 let questions = [
     {
-        question: "Commonly used data types DO NOT include:",
+        title: "Commonly used data types DO NOT include:",
         answers: [ "strings", "booleans", "alerts", "numbers"],
-        correct: "alerts"
+        correct: 2
     },
     {
-        question: "The condition in an if / else statement is enclosed within ____.",
+        title: "The condition in an if / else statement is enclosed within ____.",
         answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        correct: "parentheses"
+        correct: 3
     },
     {
-        question: "Which one would make this isNaN function true?",
+        title: "Which one would make this isNaN function true?",
         answers: ["8", "curly brackets", "string", "boolean"],
-        correct: "8"
+        correct: 0
       },
     {
-        question: "What character can break JavaScript Code into several lines?",
+        title: "What character can break JavaScript Code into several lines?",
         answers: ["backslash", "curly brackets", "parentheses", "foward slash"],
-        correct: "backlash"
+        correct: 0
     },
     {
-        question: "What company developed JavaScript?",
+        title: "What company developed JavaScript?",
         answers: ["Netscape", "Tesla", "Java", "Google"],
-        correct: "Netscape"
+        correct: 0
     },
 
   ];
 // Where I put all event listeners
-$(document).ready(function() {
+$(document).ready(function(){
 
+
+    $('.start a').click(function(e){
+        e.preventDefault();
+        $('.start').hide();
+        $('.quiz').show();
+        showQuestion();
+    });
+
+    $('.quiz ul').on('click', 'li', function(){
+        $('.selected').removeClass('selected'); 
+        $(this).addClass('selected');  
+    });
+    $('.quiz a').click(function(e){
+        e.preventDefault(); 
+        if ($('li.selected').length) {
+          let guess = parseInt($('li.selected').attr('id'));
+           checkAnswer(guess);
+        } else {
+            alert('Please Select an Answer!!!')
+        };
+
+
+    });
 });
-
-function startQuiz() {
-
-}
+ // FUNCTIONS
 
 function showQuestion () {
+    let question = questions[currentQuestion];
+    $('.quiz h2').text(question.title);
+    $('.quiz ul').html('');
+    for (var i = 0; i < question.answers.length; i++) {
+      $('.quiz ul').append("<li id= '" + i + "'>" + question.answers[i] + "</li>");  
+    }
+
 
 }
 
-function checkAnswer () {
-
+function checkAnswer (guess) {
+    let question = questions[currentQuestion];
+    if(question.correct === guess) {
+        score ++;
+    }
+    currentQuestion++;
+    if (currentQuestion >= questions.length){
+        showSummary();
+    }
+    else{ showQuestion();
+    }
+   
 }
 
 function showSummary () {
-    
+    $ ('.quiz').hide();
+    $('.summary').show();
 }
